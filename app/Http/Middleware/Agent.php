a592cebfbc08d83f0d1f87264235fc65bcda3665
santosh kumar pandit <skpandit659@gmail.com>
2021-10-21 14:48:14 +0545

    agent middleware

---
+++ app/Http/Middleware/Agent.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Auth;
+class Agent
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(!Auth::guard('web')->check()){
+            return redirect()->route('memberLogin');
+        }
+        return $next($request);
+    }
+}
